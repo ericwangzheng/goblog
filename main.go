752430a606b2787ff9b76f9a42672bbe1aa51dee
nsecgo <nsecgo@gmail.com>
2016-10-14 13:16:49 +0800

    update

--- main.go
+++ main.go
@@ -3,16 +3,8 @@ package main
 import (
 	_ "github.com/nsecgo/goblog/routers"
 	"github.com/astaxie/beego"
-	"strings"
 )
 
-func replace(in string) (out string) {
-	out = strings.Replace(in, "<p>", "", -1)
-	out = strings.Replace(out, "</p>", "", -1)
-	return
-}
-
 func main() {
-	beego.AddFuncMap("replace", replace)
 	beego.Run()
 }
